0104e60afaf6078989c53066ef36c9e2312448ef
Tyler Kellen <tyler@sleekcode.net>
2012-10-03 12:43:31 -0500

    docs

--- grunt.js
+++ grunt.js
@@ -4,7 +4,6 @@
  *
  * Copyright (c) 2012 Tim Branyen, contributors
  * Licensed under the MIT license.
- * https://github.com/gruntjs/grunt-contrib-mincss/blob/master/LICENSE-MIT
  */
 
 module.exports = function(grunt) {
