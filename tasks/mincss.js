acff2a3732a83ada3c106d64d1b896af3d6bde0d
Tyler Kellen <tyler@sleekcode.net>
2012-11-19 16:27:34 -0500

    remove unused var

--- tasks/mincss.js
+++ tasks/mincss.js
@@ -11,7 +11,7 @@
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('mincss', 'Minify CSS files', function() {
-    var srcFiles, sourceCode, sourceCompressed;
+    var sourceCode, sourceCompressed;
     var taskOutputMin = [];
     var taskOutputMax = [];
 
