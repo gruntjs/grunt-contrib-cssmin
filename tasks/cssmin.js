ea6b6c9a8932dec45e41935b6d9100bb2b5add34
banderson <bluej100@gmail.com>
2013-05-20 10:36:12 -0600

    add tests and support for @import inlining

--- tasks/cssmin.js
+++ tasks/cssmin.js
@@ -10,13 +10,14 @@
 
 module.exports = function(grunt) {
   var helper = require('grunt-lib-contrib').init(grunt);
+  var path = require('path');
 
   grunt.registerMultiTask('cssmin', 'Minify CSS files', function() {
     var options = this.options({
       report: false
     });
     this.files.forEach(function(f) {
-      var max = f.src.filter(function(filepath) {
+      var min = f.src.filter(function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
@@ -25,10 +26,12 @@ module.exports = function(grunt) {
           return true;
         }
       })
-      .map(grunt.file.read)
-      .join(grunt.util.normalizelf(grunt.util.linefeed));
+      .map(function(f) {
+        options.relativeTo = path.dirname(f);
+        return minifyCSS(grunt.file.read(f), options);
+      })
+      .join('');
 
-      var min = minifyCSS(max, options);
       if (min.length < 1) {
         grunt.log.warn('Destination not written because minified CSS was empty.');
       } else {
