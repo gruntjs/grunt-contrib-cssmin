d512bfc77467b5762900db9776e10707b431deb2
banderson <bluej100@gmail.com>
2013-05-20 16:01:15 -0600

    fix indentation

--- tasks/cssmin.js
+++ tasks/cssmin.js
@@ -27,8 +27,8 @@ module.exports = function(grunt) {
         }
       });
       var max = valid
-        .map(grunt.file.read)
-        .join(grunt.util.normalizelf(grunt.util.linefeed));
+      .map(grunt.file.read)
+      .join(grunt.util.normalizelf(grunt.util.linefeed));
       var min = valid.map(function(f) {
         options.relativeTo = path.dirname(f);
         return minifyCSS(grunt.file.read(f), options);
