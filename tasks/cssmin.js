d085cc07beb9e781bc93650dfe0db3b830686438
Kyle Robinson Young <kyle@dontkry.com>
2014-02-28 16:50:20 -0800

    Update copyright to 2014

--- tasks/cssmin.js
+++ tasks/cssmin.js
@@ -2,7 +2,7 @@
  * grunt-contrib-cssmin
  * http://gruntjs.com/
  *
- * Copyright (c) 2013 Tim Branyen, contributors
+ * Copyright (c) 2014 Tim Branyen, contributors
  * Licensed under the MIT license.
  */
 
