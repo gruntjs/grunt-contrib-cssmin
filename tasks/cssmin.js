782525103547224b70a2cb29371980b0eea7a337
Levente Balogh <levente.balogh@balabit.com>
2015-02-06 16:44:52 +0100

    Added source map support for CleanCSS minifying.
    Signed-off-by: Levente Balogh <balogh.levente.hu@gmail.com>

--- tasks/cssmin.js
+++ tasks/cssmin.js
@@ -2,7 +2,7 @@
  * grunt-contrib-cssmin
  * http://gruntjs.com/
  *
- * Copyright (c) 2015 Tim Branyen, contributors
+ * Copyright (c) 2014 Tim Branyen, contributors
  * Licensed under the MIT license.
  */
 
@@ -13,46 +13,54 @@ var chalk = require('chalk');
 var maxmin = require('maxmin');
 
 module.exports = function(grunt) {
-  var minify = function(source, options) {
-    try {
-      return new CleanCSS(options).minify(source).styles;
-    } catch (err) {
-      grunt.log.error(err);
-      grunt.fail.warn('CSS minification failed.');
-    }
-  };
 
-  grunt.registerMultiTask('cssmin', 'Minify CSS', function() {
-    var options = this.options({
-      report: 'min'
-    });
-
-    this.files.forEach(function(file) {
-      var valid = file.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file ' + chalk.cyan(filepath) + ' not found.');
-          return false;
-        } else {
-          return true;
+    var minifyCssFiles = function (options, files) {
+        try {
+            return new CleanCSS(options).minify(files);
+        } catch (err) {
+            grunt.log.error(err);
+            grunt.fail.warn('CSS minification failed.');
         }
-      });
+    };
 
-      var max = '';
-      var min = valid.map(function(file) {
-        var src = grunt.file.read(file);
-        max += src;
-        options.relativeTo = path.dirname(file);
-        return minify(src, options);
-      }).join('');
+    var getAvailableFiles = function (filesArray) {
+        return filesArray.filter(function(filepath) {
+            if (!grunt.file.exists(filepath)) {
+                grunt.log.warn('Source file ' + chalk.cyan(filepath) + ' not found.');
+                return false;
+            } else {
+                return true;
+            }
+        });
+    };
 
-      if (min.length === 0) {
-        return grunt.log.warn('Destination not written because minified CSS was empty.');
-      }
+    grunt.registerMultiTask('cssmin', 'Minify CSS files with CleanCSS.', function() {
+        var options = this.options({
+          report: 'min',
+          sourceMap: false,
+        });
+        this.files.forEach(function(file) {
+            var availableFiles = getAvailableFiles(file.src);
+            var cleanCssOptions = { sourceMap: true, target: file.dest };
+            var compiled = minifyCssFiles(cleanCssOptions, availableFiles);
+            var compiledCssString = compiled.styles;
 
-      grunt.file.write(file.dest, min);
+            if (compiledCssString.length === 0) {
+              return grunt.log.warn('Destination not written because minified CSS was empty.');
+            }
 
-      grunt.verbose.writeln('File ' + chalk.cyan(file.dest) + ' created: ' + maxmin(max, min, options.report === 'gzip'));
+            //source map
+            if (options.sourceMap) {
+                var compiledFileName = file.dest.split('/').pop();
+                var sourceMapString = compiled.sourceMap.toString();
+                compiledCssString += '\n' + '/*# sourceMappingURL='+compiledFileName+'.map */';
+                grunt.file.write(file.dest+'.map', sourceMapString);
+            }
+
+            //write compiled css file
+            grunt.file.write(file.dest, compiledCssString);
+
+            grunt.verbose.writeln('File ' + chalk.cyan(file.dest) + ' created.');
+        });
     });
-  });
 };
--- test/cssmin_test.js
+++ test/cssmin_test.js
@@ -5,8 +5,8 @@ exports.cssmin = {
   main: function(test) {
     test.expect(1);
 
-    var expect = grunt.file.read('test/expected/style.css');
-    var result = grunt.file.read('tmp/style.css');
+    var expect = grunt.file.read('test/expected/style.css').replace(/\n/g,'');
+    var result = grunt.file.read('tmp/style.css').replace(/\n/g,'');
     test.equal(expect, result, 'should concat and minify an array of css files in order using clean-css');
 
     test.done();
@@ -21,8 +21,8 @@ exports.cssmin = {
   imports: function(test) {
     test.expect(1);
 
-    var expect = grunt.file.read('test/expected/inline_import.css');
-    var result = grunt.file.read('tmp/inline_import.css');
+    var expect = grunt.file.read('test/expected/inline_import.css').replace(/\n/g,'');
+    var result = grunt.file.read('tmp/inline_import.css').replace(/\n/g,'');
     test.equal(expect, result, 'should inline @import');
 
     test.done();
