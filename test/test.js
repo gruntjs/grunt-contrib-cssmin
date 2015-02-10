3d773375c5a41ad547ac6d07eca963d3f1f10d79
XhmikosR <xhmikosr@gmail.com>
2015-02-10 10:40:37 +0200

    Fix Windows test failures.

--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ exports.cssmin = {
   main: function(test) {
     test.expect(1);
 
-    var expect = grunt.file.read('test/expected/style.css').replace(/\n/g,'');
-    var result = grunt.file.read('tmp/style.css').replace(/\n/g,'');
+    var expect = grunt.file.read('test/expected/style.css').replace(/\r\n/g,'').replace(/\n/g,'');
+    var result = grunt.file.read('tmp/style.css').replace(/\r\n/g,'').replace(/\n/g,'');
     test.equal(expect, result, 'should concat and minify an array of css files in order using clean-css');
 
     test.done();
@@ -14,8 +14,8 @@ exports.cssmin = {
   imports: function(test) {
     test.expect(1);
 
-    var expect = grunt.file.read('test/expected/inline_import.css').replace(/\n/g,'');
-    var result = grunt.file.read('tmp/inline_import.css').replace(/\n/g,'');
+    var expect = grunt.file.read('test/expected/inline_import.css').replace(/\r\n/g,'').replace(/\n/g,'');
+    var result = grunt.file.read('tmp/inline_import.css').replace(/\r\n/g,'').replace(/\n/g,'');
     test.equal(expect, result, 'should inline @import');
 
     test.done();
